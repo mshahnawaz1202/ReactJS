978de7529d9ce279b4f137a4d106b347f700b672
Muhammad Shah Nawaz <shahnawaz.swz1202@gmail.com>
2026-07-07 12:54:49 +0500

    Create your own react library and JSX

--- 01ViteProject/src/App.jsx
+++ 01ViteProject/src/App.jsx
@@ -2,13 +2,17 @@ import Superman from "./superman"
 
 function App() {
 
+  const username = 'Superman'
   return (
       // <h1>React with Vite | M.Shah Nawaz</h1>
       // fragment fro returning multiple tags
+
+      
+
       <> 
       <Superman/>
       <h2>React+Vite</h2>
-      <p>React App using Vite</p>
+      <p>React App using Vite {username}</p>
       </>
   )
 }
--- 01ViteProject/src/main.jsx
+++ 01ViteProject/src/main.jsx
@@ -1,10 +1,36 @@
-import { StrictMode } from 'react'
+// import React from 'react'
+// import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
-import App from './App.jsx'
+// import App from './App.jsx'
+
+// const user = 'shahnawaz'
+
+// const anotherElement = (
+//    <a href="http://google.com">Visit Google</a>
+// )
+
+
+// const ReactElement = React.createElement('a',{
+//         href : 'https://google.com',
+//         target : '_blank'
+//     },'click to visit google',user)
+
+
+
+
+
+export default function MyApp(){
+  return(
+    <h1>function my app</h1>
+  )
+}
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  // anotherElement
+  // ReactElement
+  // <App/>
+  <MyApp/>
+  // MyApp()
+
 )
