9d8f3183c794dd45d912777a116f56e288833d4d
Muhammad Shah Nawaz <shahnawaz.swz1202@gmail.com>
2026-07-07 10:27:35 +0500

    refactor: streamline customRender function by removing commented-out code and enhancing attribute setting

--- custom_react/custom_react.js
+++ custom_react/custom_react.js
@@ -1,12 +1,23 @@
    
 
 function customRender(element,root){
-    const domElement = document.createElement(element.type);
-    domElement.innerHTML =  element.children;
-    domElement.setAttribute('href',element.props.href);
-    domElement.setAttribute('target',element.props.target);
+    // const domElement = document.createElement(element.type);
+    // domElement.innerHTML =  element.children;
+    // domElement.setAttribute('href',element.props.href);
+    // domElement.setAttribute('target',element.props.target);
 
-    root.appendChild(domElement);
+    // root.appendChild(domElement);
+
+
+    const domElement = document.createElement(element.type)
+    domElement.innerHTML = element.children
+    for (const prop in element.props) {
+       if(prop === 'children') continue
+       domElement.setAttribute(prop,element.props[prop])
+
+        
+    }
+    root.appendChild(domElement)
 
 
 }
