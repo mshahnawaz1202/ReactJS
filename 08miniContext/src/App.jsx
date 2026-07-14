673ba14f31bbe3eb7543560c4c70210daa70c410
Muhammad Shah Nawaz <shahnawaz.swz1202@gmail.com>
2026-07-14 14:14:05 +0500

    feat: add Login component for user authentication and update App component with UserContextProvider

--- 08miniContext/src/App.jsx
+++ 08miniContext/src/App.jsx
@@ -1,14 +1,14 @@
 
-
+import UserContextProvider from './context/UserContextProvider'
 
 
 function App() {
   
 
   return (
-    <>
+    <UserContextProvider>
       <h1>React JS</h1>
-    </>
+    </UserContextProvider>
   )
 }
 
