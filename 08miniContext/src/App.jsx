203536f0998da734e845bc511ed92190d405c44c
Muhammad Shah Nawaz <shahnawaz.swz1202@gmail.com>
2026-07-15 13:41:42 +0500

    feat: add Login and Profile components to App for user interaction

--- 08miniContext/src/App.jsx
+++ 08miniContext/src/App.jsx
@@ -1,6 +1,7 @@
 
 import UserContextProvider from './context/UserContextProvider'
-
+import Login from './components/Login'
+import Profile from './components/Profile'
 
 function App() {
   
@@ -8,6 +9,8 @@ function App() {
   return (
     <UserContextProvider>
       <h1>React JS</h1>
+      <Login/>
+      <Profile/>
     </UserContextProvider>
   )
 }
