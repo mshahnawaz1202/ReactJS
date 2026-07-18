4394aace3eab8da4e3858dde961a7ae5341f761d
Muhammad Shah Nawaz <shahnawaz.swz1202@gmail.com>
2026-07-18 12:00:23 +0500

    refactor: clean up todoSlice.js by improving formatting and removing unnecessary comments

--- reduxToolkitTodo/src/features/todo/todoSlice.js
+++ reduxToolkitTodo/src/features/todo/todoSlice.js
@@ -1,40 +1,36 @@
 import { createSlice, nanoid } from "@reduxjs/toolkit";
 
-
-// initial state can be an object or an array
-
 const initialState = {
-    todos: [{ id: 1, text: 'Hello World!' }]
-}
+    todos: [
+        {
+            id: 1,
+            text: "Hello World!"
+        }
+    ]
+};
 
 export const todoSlice = createSlice({
-    name: 'todo',
+    name: "todo",
     initialState,
+
     reducers: {
         addTodo: (state, action) => {
             const todo = {
                 id: nanoid(),
-                text: action.payload.text
-            }
-            state.todos.push(todo)
-        },
-        removeTodo: (action,state) => {
-            state.todos = state.todos.filter((todo) => todo.id !== action.payload )
+                text: action.payload
+            };
+
+            state.todos.push(todo);
         },
 
+        removeTodo: (state, action) => {
+            state.todos = state.todos.filter(
+                (todo) => todo.id !== action.payload
+            );
+        },
     },
+});
 
-})
+export const { addTodo, removeTodo } = todoSlice.actions;
 
-
-
-
-
-export const {addTodo, removeTodo} = todoSlice.actions // for components 
-
-export default todoSlice.reducer  // for store to access each reducer
-
-
-
-
-
+export default todoSlice.reducer;
