a65a704d55a4bbf2e55b37c0c4a78b0ef10c5c40
Muhammad Shah Nawaz <shahnawaz.swz1202@gmail.com>
2026-07-18 11:59:46 +0500

    feat: implement addTodoHandler function to handle form submission in AddTodo component

--- reduxToolkitTodo/src/components/AddTodo.jsx
+++ reduxToolkitTodo/src/components/AddTodo.jsx
@@ -1,11 +1,18 @@
 import React, { useState } from 'react'
 import {useDispatch} from 'react-redux'
+import {addTodo} from '../features/todo/todoSlice'
 
 function AddTodo() {
 
   const [input,setInput] = useState('')
   const dispatch = useDispatch();
 
+  const addTodoHandler = (e) =>{
+    e.preventDefault()
+    dispatch(addTodo(input))
+    setInput('')
+
+  }
 
     
   return (
